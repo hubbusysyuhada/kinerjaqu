3d46c2409975661603fe90a1bdf24f93c41081eb
hubbusysyuhada <hubbusysyuhada@gmail.com>
2021-08-12 05:54:37 +0700

    deploy heroku fixing

--- client/src/helpers/axios.js
+++ client/src/helpers/axios.js
@@ -1,5 +1,6 @@
 import asyncAxios from 'axios'
 
+// const baseURL = 'https://kinerjaqu.herokuapp.com/'
 const baseURL = 'http://localhost:3000'
 
 export default asyncAxios.create({
--- server/helpers/confirmation.js
+++ server/helpers/confirmation.js
@@ -1,33 +1,36 @@
 const nodemailer = require('nodemailer')
 
 async function sendConfirmationEmail (email, code) {
-    let transporter = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-            user: process.env.GOOGLE_EMAIL,
-            pass: process.env.GOOGLE_PASSWORD
-        }
-    })
-    const sendEmail = await transporter.sendMail({
-        from: process.env.GOOGLE_EMAIL,
-        to: email,
-        subject: 'Kinerjaqu Email Verification',
-        html: `Welcome to <b>Kinerjaqu</b>
-        <br>
-        <br>
-        Your activation code is <b>${code}</b>
-        <br>
-        <br>
-        Glad to welcome you =)
-        <br>
-        <br>
-        <b>Kinerjaqu Team</b>
-        <br>
-        <br>
-        For further inquirires: <a href="https://www.linkedin.com/in/hubbusysyuhada">Linkedin</a>, <a href="https://github.com/hubbusysyuhada">Github</a>, or <a href="wa.me/+6282233197540">Whatsapp</a>
-        `
-    })
-    console.log(sendEmail)
+    try {
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.GOOGLE_EMAIL,
+                pass: process.env.GOOGLE_PASSWORD
+            }
+        })
+        const sendEmail = await transporter.sendMail({
+            from: process.env.GOOGLE_EMAIL,
+            to: email,
+            subject: 'Kinerjaqu Email Verification',
+            html: `Welcome to <b>Kinerjaqu</b>
+            <br>
+            <br>
+            Your activation code is <b>${code}</b>
+            <br>
+            <br>
+            Glad to welcome you =)
+            <br>
+            <br>
+            <b>Kinerjaqu Team</b>
+            <br>
+            <br>
+            For further inquirires: <a href="https://www.linkedin.com/in/hubbusysyuhada">Linkedin</a>, <a href="https://github.com/hubbusysyuhada">Github</a>, or <a href="wa.me/+6282233197540">Whatsapp</a>
+            `
+        })
+    } catch (error) {
+        console.log(error, '<<< confirmation error');
+    }
 }
 
 module.exports = {
